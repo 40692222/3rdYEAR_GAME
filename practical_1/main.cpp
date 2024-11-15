8051fffa8c17c3b3759f030ef22fe75e4d1f8a6e
Woodbridge <40646743@live.napier.ac.uk>
2024-11-15 17:38:16 +0000

    Added two rectangles to the screen for main menu

--- practical_1/main.cpp
+++ practical_1/main.cpp
@@ -1,9 +1,30 @@
 #include <SFML/Graphics.hpp>
 
 int main(){
-  sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+
+  unsigned int width = 1300;
+  unsigned int height = 200;
+
+  sf::RenderWindow window(sf::VideoMode({1280, 720}), "SFML works!");
+  
+  // Define the rectangle's size
+  float rectWidth = 400.f;
+  float rectHeight = 100.f;
+  float spacing = 50.f;
+
+
+  // Create three rectangle shapes with the same size
+    sf::RectangleShape rectangle1(sf::Vector2f(rectWidth, rectHeight));
+    rectangle1.setFillColor(sf::Color(100, 100, 100));  // Set color to a darker gray
+    rectangle1.setPosition((width - rectWidth) / 2, (height - 3 * rectHeight - 2 * spacing) / 2);  // Top rectangle
+
+    sf::RectangleShape rectangle2(sf::Vector2f(rectWidth, rectHeight));
+    rectangle2.setFillColor(sf::Color(100, 100, 100));  // Set color to a darker gray
+    rectangle2.setPosition((width - rectWidth) / 2, (height - 3 * rectHeight - 2 * spacing) / 2 + rectHeight + spacing);  // Middle rectangle
+
+    sf::RectangleShape rectangle3(sf::Vector2f(rectWidth, rectHeight));
+    rectangle3.setFillColor(sf::Color(100, 100, 100));  // Set color to a darker gray
+    rectangle3.setPosition((width - rectWidth) / 2, (height - 3 * rectHeight - 2 * spacing) / 2 + 2 * (rectHeight + spacing));  // Bottom rectangle
 
   while (window.isOpen()){
       sf::Event event;
@@ -12,9 +33,15 @@ int main(){
         window.close();
       }
     }
-    window.clear();
-    window.draw(shape);
-    window.display();
+      window.clear(sf::Color::Black);
+
+      // Draw the rectangle
+      window.draw(rectangle1);
+      window.draw(rectangle2);
+      window.draw(rectangle3);
+
+      // Display the contents of the window
+      window.display();
   }
   return 0;
 }
